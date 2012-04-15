8b11322b3f286c0e68ece40a2097d6ca3e61202c
Edward Norris <etnc6d@mst.edu>
2012-04-15 18:26:23 -0500

    Added the flag graphics

--- src/k/android/hellolocalization/MainActivity.java
+++ src/k/android/hellolocalization/MainActivity.java
@@ -1,7 +1,11 @@
 package k.android.hellolocalization;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +13,38 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Button b;
+        b = (Button)findViewById(R.id.button_id);
+        b.setBackgroundDrawable(this.getResources().getDrawable(R.drawable.usflagpng));
+        
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.dialog1_text).
+        	setCancelable(false).setTitle(R.string.dialog1_title).
+        	setPositiveButton("Done", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int id) {
+					dialog.dismiss();
+				}
+			});
+        final AlertDialog alert = builder.create();
+        
+        b.setOnClickListener(new View.OnClickListener(){
+        	public void onClick(View v){
+        		alert.show();
+        	}
+        });
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
