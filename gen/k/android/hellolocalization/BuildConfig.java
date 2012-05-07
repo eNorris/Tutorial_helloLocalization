0c2586701463ec9934afbabffade1f6c98c168ba
enorris <812xi@gmail.com>
2012-05-07 11:58:45 -0500

    Finished localization tutorial

---
+++ gen/k/android/hellolocalization/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package k.android.hellolocalization;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- gen/k/android/hellolocalization/R.java
+++ gen/k/android/hellolocalization/R.java
@@ -11,15 +11,12 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int franceflag=0x7f020000;
-        public static final int franceflagpng=0x7f020001;
+        public static final int flag=0x7f020000;
+        public static final int franceflag=0x7f020001;
         public static final int germanyflag=0x7f020002;
-        public static final int germanyflagpng=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
-        public static final int japanflag=0x7f020005;
-        public static final int japanflagpng=0x7f020006;
-        public static final int usflag=0x7f020007;
-        public static final int usflagpng=0x7f020008;
+        public static final int ic_launcher=0x7f020003;
+        public static final int japanflag=0x7f020004;
+        public static final int usflag=0x7f020005;
     }
     public static final class id {
         public static final int button_id=0x7f050000;
@@ -28,11 +25,10 @@ public final class R {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int dialog1_text=0x7f040005;
-        public static final int dialog1_title=0x7f040004;
-        public static final int hello=0x7f040000;
-        public static final int textView1_str=0x7f040002;
-        public static final int textView2_str=0x7f040003;
+        public static final int app_name=0x7f040000;
+        public static final int dialog1_text=0x7f040004;
+        public static final int dialog1_title=0x7f040003;
+        public static final int textView1_str=0x7f040001;
+        public static final int textView2_str=0x7f040002;
     }
 }
